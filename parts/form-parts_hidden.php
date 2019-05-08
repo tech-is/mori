d006a7ff2d87f4d883412e24e7d8cd22c9da9039
Admin <Cipher_GALM01@outlook.jp>
2019-05-08 16:07:41 +0900

    master

--- parts/form-parts_hidden.php
+++
@@ -1,5 +0,0 @@
-<div class ="form-group">
-    <input type="hidden" 
-        name="<?= echo $val[0]; ?>"
-        value="<?= echo $val[1]; ?>">
-</div>
--- parts/sql.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if(!empty($_POST["submit"])){
-    $sql = "INSERT INTO MEMBER ($key) VALUES (:$key)";
-    $stmt = $pdo->prepare($sql);
-    $stmt -> bindValue(":$key", $_POST[$key], PDO::PARAM_STR);
-    $stmt -> execute();
-    }
-?>
